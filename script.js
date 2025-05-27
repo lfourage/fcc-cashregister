39bde8f2d3d5a5c67457f360abff055c5e8685d2
lfourage <ludogriph@gmail.com>
2025-05-27 19:10:52 +0200

    seems to work, not tested yet

--- script.js
+++ script.js
@@ -30,29 +30,36 @@ const hasEnoughCash = (difference) => {
   return difference <= sumOfAmounts;
 };
 
-const getFromRegister = (customerCash) => {
+const isRegisterEmpty = () => !(cid.some((el) => el[amount]));
+
+const getFromRegister = (changeToPay) => {
   const currencyType = cid
-    .find((el) => el[value] <= customerCash - price && el[amount]);
+    .find((el) => el[value] <= changeToPay && el[amount]);
 
   let amountTaken = 0;
-  while (currencyType[value] <= customerCash - price && currencyType[amount]) {
-    customerCash -= currencyType[value];
-    currencyType[amount] -= currencyType[value];
-    amountTaken += currencyType[value];
+
+  while (currencyType[value] <= changeToPay && currencyType[amount]) {
+    changeToPay = parseFloat((changeToPay - currencyType[value]).toFixed(2));
+    currencyType[amount] = parseFloat((currencyType[amount] - currencyType[value]).toFixed(2));
+    amountTaken = parseFloat((amountTaken +currencyType[value]).toFixed(2));
   }
-  if (customerCash)
-    return `${currencyType[str]}: \$${amountTaken}\n${getFromRegister(customerCash)}`;
+  if (changeToPay)
+    return `${currencyType[str]}: \$${amountTaken}\n${getFromRegister(changeToPay)}`;
   return `${currencyType[str]}: \$${amountTaken}`;
 };
 
 const clickPurchaseButton = () => {
-  const customerCash = Number(cash.value);
+  const customerCash = parseFloat(cash.value);
 
   if (customerCash >= price) {
+    const changeToPay = parseFloat((customerCash - price).toFixed(2));
     if (customerCash === price)
       changeDue.innerText = "No change due - customer paid with exact cash";
-    else if (hasEnoughCash(customerCash - price)) {
-      changeDue.innerText = `Status: ${getFromRegister(customerCash)}`;
+    else if (hasEnoughCash(changeToPay)) {
+      const taken = getFromRegister(changeToPay);
+      const status = isRegisterEmpty() ? "CLOSED" : "OPEN";
+
+      changeDue.innerText = `Status: ${status}\n${taken}`;
     } else changeDue.innerText = "Status: INSUFFICIENT_FUNDS";
   } else alert("Customer does not have enough money to purchase the item");
   cash.value = "";
